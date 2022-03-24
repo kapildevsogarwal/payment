6f64a9201f67d06e414bb215c7a8bee288cd8687
kapildevsogarwal <kapildevsogarwal@gmail.com>
2022-03-24 16:15:59 +0530

    add changes for redirect aspes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -19,7 +20,10 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+   // use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        logout as performLogout;
+    }
 
     /**
      * Where to redirect users after login.
@@ -27,7 +31,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-
+    //protected $redirectTo = 'https://aspes.in/';
     /**
      * Create a new controller instance.
      *
@@ -37,4 +41,15 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function logout(Request $request)
+{
+    $this->guard()->logout();
+
+    $request->session()->flush();
+
+    $request->session()->regenerate();
+
+    return redirect('https://aspes.in');
+}
 }
